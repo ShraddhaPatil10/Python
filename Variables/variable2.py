6f11f3d8a99a33e1959745462c5d2d2a39ed84f3
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-10-04 15:52:49 +0530

    Add files via upload

---
+++ Variables/variable2.py
@@ -0,0 +1,25 @@
+print("********************************************")
+no=11
+print("Value of no=",no)
+print("Data type of no:",type(no))
+print("Id of no=",id(no))
+
+print("*************************************************")
+no=45.65
+print("Value of no=",no)
+print("Data type of no:",type(no))
+print("Id of no=",id(no))
+
+print("**************************************************")
+no=True
+print("Value of no=",no)
+print("Data type of no:",type(no))
+print("Id of no=",id(no))
+
+print("****************************************************")
+no="Hello"
+print("Value of no=",no)
+print("Data type of no:",type(no))
+print("Id of no=",id(no))
+
+print("*******************************************************")
