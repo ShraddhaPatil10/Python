b67d06eaca9ed49e38ca03b805222eed3b337227
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-10-04 07:47:46 +0530

    Add files via upload

---
+++ My_first_program_2nd_approach.py
@@ -0,0 +1,6 @@
+#print Jay Ganesh on screen using entry point function
+
+def main():
+    print("Jay Ganesh!!!")
+
+main()
---
+++ My_first_program_3rd_approach.py
@@ -0,0 +1,10 @@
+#Print Jay Ganesh on screen using starter and entry point function
+def main():
+    print("Jay Ganesh!!!")
+    print("Inside main")
+
+if __name__=="__main__":
+    print("Inside Satrter")
+    main()
+
+print("End of my program.")
