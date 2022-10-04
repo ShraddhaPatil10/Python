08ad236e91ee43cc414c7d193100b648423edcb9
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-10-04 15:39:56 +0530

    Add files via upload

---
+++ Variables/variables.py
@@ -0,0 +1,17 @@
+print("Demonstration of variable creation")
+
+no=11
+print("Value of no=",no)
+print("Data type of no:",type(no))
+
+marks=89.65
+print("Value of marks=",marks)
+print("Data type of marks:",type(marks))
+
+mobile=True
+print("Value of mobile=",mobile)
+print("Data type of mobile:",type(mobile))
+
+city="Pune"
+print("Value of city=",city)
+print("Data type of city:",type(city))
