6ef84491cdf037e6a3e43213964397337349733e
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-10-04 15:45:07 +0530

    Add files via upload

---
+++ Variables/variables_and_ID.py
@@ -0,0 +1,21 @@
+print("*********************************")
+no=11
+print("Value of no is=",no)
+print("Data type of no:",type(no))
+print("ID of no is:",id(no))
+
+print("*************************************")
+
+i=11
+print("Value of i is=",i)
+print("Data type of i:",type(i))
+print("ID of i:",id(i))
+
+print("**************************************")
+
+num=10
+print("Value of num=",num)
+print("Data type of num:",type(num))
+print("ID of num:",id(num))
+
+print("***************************************")
