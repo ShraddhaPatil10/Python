26adcccdc766227a5aa693eb5421360009bf56c8
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2022-10-11 20:52:00 +0530

    Add files via upload

---
+++ Loops/Print_Hello_5_times_using_for.py
@@ -0,0 +1,9 @@
+def main():
+    for i in range(1,6,1):
+        print("Hello!!!")
+
+    for j in range(1,10,2):
+        print(j)
+
+if __name__=="__main__":
+    main()
---
+++ Loops/while_loop_demo.py
@@ -0,0 +1,8 @@
+def main():
+    i=1
+    while i<11:
+        print(i,end=" ")
+        i+=1
+
+if __name__=="__main__":
+    main()
