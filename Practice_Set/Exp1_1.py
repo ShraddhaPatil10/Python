d0158e5174ba63ed168698104a0a3ea756379b8a
ShraddhaPatil10 <102403129+ShraddhaPatil10@users.noreply.github.com>
2024-02-14 11:42:30 +0530

    Add files via upload

---
+++ Practice_Set/Exp1_1.py
@@ -0,0 +1,28 @@
+#Data types in python
+def main():
+    no=10
+    print("Given value is:",no)
+    print("Data type of no:",type(no))
+    print("")
+
+    no=10.25
+    print("Given value is:",no)
+    print("Data type of no:",type(no))
+    print("")
+
+    no="Shraddha"
+    print("Given value is:",no)
+    print("Data type of no:",type(no))
+    print("")
+    
+    no=True
+    print("Given value is:",no)
+    print("Data type of no:",type(no))
+    print("")
+
+    no=3 + 7j
+    print("Given value is:",no)
+    print("Data type of no:",type(no))
+
+if __name__=="__main__":
+    main()
